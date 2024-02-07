9f0fb037d1f57707840b5e64d205e40996b9268f
abderrahmane <aabderrahmane665@gmail.com>
2024-02-07 18:10:38 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useState } from 'react';
 
 function App() {
+
+
+  const [todos,setTodos]=useState([]);
+  const inpref =useRef();
+  
+  const todohandler =() => {
+  const text =  inpref.current.value;
+  setTodos([...todos,text]);
+  inpref.current.value="";
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       <h1>TO DO LIST</h1>
+       <ul>
+          {todos.map((item) => {
+            return <li>{item}</li> ;
+          })}
+       </ul>
+       <div className="inputadd">
+       <input ref={inpref} type="text" placeholder='write here' />
+       <button onClick={todohandler}>Add</button>
+       </div>
     </div>
   );
 }
