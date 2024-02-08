97d627cfdd88419e7b9d2502cc673aecd1c8a123
abderrahmane <aabderrahmane665@gmail.com>
2024-02-08 12:25:49 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import './App.css';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function App() {
-
-
-  const [todos,setTodos]=useState([]);
+  
+  const [todos,setTodos]=useState( JSON.parse(localStorage.pro));
   const inpref =useRef();
   
+  useEffect(() =>{
+    localStorage.setItem('pro', JSON.stringify(todos));
+
+  },[todos]);
+  
+  
+    
   const todohandler =() => {
   const text =  inpref.current.value;
-  setTodos([...todos,text]);
-  inpref.current.value="";
+  const newItmes= { complted:false , text}
+  setTodos([...todos,newItmes]);
+  inpref.current.value="";  
 };
+const  donehandler =(index) => {
+  const newtodos=[...todos];
+  newtodos[index].complted=!newtodos[index].complted;
+  setTodos(newtodos);
+
+};
+const delatehandler=(index) => {
+  const supptodos=[...todos];
+  supptodos.splice(index, 1);
+  setTodos(supptodos);
+}
 
   return (
     <div className="App">
-      
+        
        <h1>TO DO LIST</h1>
        <ul>
-          {todos.map((item) => {
-            return <li>{item}</li> ;
+        <table>
+          {
+          todos.map((item , index) => {
+            return <tr>
+            <td> <li   className={item.complted? "done" : ""}
+             key={index}
+              onClick={()=> donehandler(index)}>{item.text}</li></td>
+              <td><span onClick={()=> delatehandler(index)} >❌</span></td>
+              </tr> ;
           })}
+          </table>
        </ul>
        <div className="inputadd">
        <input ref={inpref} type="text" placeholder='write here' />
